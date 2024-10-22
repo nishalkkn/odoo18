c577a9225a8a6efae89331dde4931fd1b47d3040
Nishal <nishalkkn@gmail.com>
2024-10-22 16:35:54 +0530

    oct 22

---
+++ employee_dashboard/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Employee Dashboard',
+    'version': '18.0.1.0.1',
+    'application': True,
+    'summary': 'Employee Dashboard',
+    'description': """
+This module provide the employee dashboard
+""",
+
+    'depends': [
+        'base',
+        'hr',
+    ],
+
+    'data': [],
+
+    'license': 'LGPL-3',
+}
