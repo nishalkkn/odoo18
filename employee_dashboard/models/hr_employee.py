79badf54fefca5e4dab4abcbd29b6f77f33f7058
Nishal <nishalkkn@gmail.com>
2024-10-28 17:23:16 +0530

    oct 28

--- employee_dashboard/models/hr_employee.py
+++ employee_dashboard/models/hr_employee.py
@@ -9,6 +9,15 @@ class HrAttendance(models.Model):
     @api.model
     def get_tiles_data(self):
         """ Return the tile data"""
+
+        employees = self.search([])
+        employee_data = []
+        for employee in employees:
+            employee_data.append({
+                'name': employee.name,
+                'id': employee.id,
+            })
+
         current_employee = self.search([('user_id', '=', self.env.user.id)])
         attendance_count = self.env['hr.attendance'].search_count(
             [('employee_id', '=', current_employee.id)]) if current_employee else 0
@@ -17,6 +26,7 @@ class HrAttendance(models.Model):
         project_count = self.env['project.project'].search_count([('user_id', '=', self.env.user.id)])
         years_of_experience = ((datetime.today() - current_employee.create_date).days) // 365
         return {
+            'employees': employee_data,
             'current_employee_id': current_employee.id,
             'employee_job_title': current_employee.job_id.name,
             'years_of_experience': years_of_experience,
@@ -24,24 +34,3 @@ class HrAttendance(models.Model):
             'leave_count': leave_count,
             'project_count': project_count,
         }
-
-    @api.model
-    def get_org_chart_data(self):
-        """ Return the organization chart data for the current employee"""
-        current_employee = self.search([('user_id', '=', self.env.user.id)])
-        if current_employee:
-            parent_id = current_employee.parent_id
-            child_ids = self.search([('parent_id', '=', current_employee.id)])
-            if parent_id:
-                org_chart_html = f"<div style='color: #bf80ff;'>{parent_id.name}</div>"
-                org_chart_html += f"<div style='margin-left: 20px;' class='o_treeEntry py-2'>{current_employee.name}</div>"
-                org_chart_html += ''.join(
-                    f"<div style='margin-left: 40px; color: #bf80ff;' class='o_treeEntry py-2'>{rec.name}</div>"
-                    for rec in child_ids)
-                return org_chart_html
-            org_chart_html = f"<div>{current_employee.name}</div>"
-            org_chart_html += ''.join(
-                f"<div style='margin-left: 20px; color: #bf80ff;' class='o_treeEntry py-2'>{rec.name}</div>" for
-                rec in child_ids)
-            return org_chart_html
-        return ""
--- employee_dashboard/static/src/js/dashboard.js
+++ employee_dashboard/static/src/js/dashboard.js
@@ -1,7 +1,7 @@
 /**@odoo-module **/
 import { registry } from "@web/core/registry";
 import { useService } from "@web/core/utils/hooks";
-import { Component } from  "@odoo/owl";
+import { Component, useState } from  "@odoo/owl";
 import { user } from "@web/core/user";
 
 const actionRegistry = registry.category("actions");
@@ -11,28 +11,80 @@ class EmployeeDashboard extends Component {
         this.orm = useService('orm');
         this.current_employee_id = null;
         this._fetch_data();
+
+        this.state = useState({
+            value: {},
+        });
     }
 
-    _fetch_data() {
+    //    function for fetching data from model
+    async _fetch_data() {
         var self = this;
-        this.orm.call("hr.employee", "get_tiles_data", [], {}).then(function(result) {
+        var employees = null;
+        await this.orm.call("hr.employee", "get_tiles_data", [], {}).then(function(result) {
+            employees = result.employees;
+
             self.current_employee_id = result.current_employee_id;
             document.getElementById('attendance_count').append(result.attendance_count);
             document.getElementById('leave_count').append(result.leave_count);
             document.getElementById('project_count').append(result.project_count);
             document.getElementById('employee_name').append(user.name);
             document.getElementById('employee_job_title').append(result.employee_job_title);
-            document.getElementById('year_of_experience').append('Experience : ', result.years_of_experience,' Years');
+            document.getElementById('year_of_experience').append('Experience : ', result.years_of_experience, ' Years');
 
             self._fetch_org_chart(self.current_employee_id);
         });
+        this.state.value = employees
     }
 
-    _fetch_org_chart() {
-        this.orm.call("hr.employee", "get_org_chart_data", [], {}).then(function(orgChart) {
-            document.getElementById('organization_chart').innerHTML += orgChart;
-        });
+    //    function for fetch org chart data
+    async _fetch_org_chart() {
+        const current_employee = await this.orm.searchRead('hr.employee', [
+            ['user_id', '=', user.userId]
+        ], ['id', 'name']);
+        if (current_employee.length > 0) {
+            const current_employee_id = current_employee[0].id;
+            const current_employee_name = current_employee[0].name;
+            const parent = await this.orm.searchRead('hr.employee', [
+                ['id', '=', current_employee_id]
+            ], ['parent_id']);
+            let parent_name = '';
+            if (parent.length > 0) {
+                parent_name = parent[0].parent_id[1];
+            }
+            const children = await this.orm.searchRead('hr.employee', [
+                ['parent_id', '=', current_employee_id]
+            ], ['id', 'name']);
+            let org_chart_html = '';
+            if (parent_name) {
+                org_chart_html += `<div style='color: #bf80ff;'>${parent_name}</div>`;
+            }
+            org_chart_html += `<div style='margin-left: 20px;' class='o_treeEntry py-2'>${current_employee_name}</div>`;
+            if (children.length > 0) {
+                for (const child of children) {
+                    const child_name = child.name;
+                    org_chart_html += `<div style='margin-left: 40px; color: #bf80ff;' class='o_treeEntry py-2'>${child_name}</div>`;
+                }
+            }
+            document.getElementById('organization_chart').innerHTML += org_chart_html;
+        }
     }
+
+//    attendanceTileClicked(employ_id = null) {
+//        const employeeId = employ_id || this.current_employee_id;
+//        this.env.services.action.doAction({
+//            type: "ir.actions.act_window",
+//            res_model: "hr.attendance",
+//            domain: [
+//                ['employee_id', '=', employeeId]
+//            ],
+//            views: [
+//                [false, "list"],
+//                [false, "form"]
+//            ],
+//            target: 'current',
+//        });
+//    }
 
     attendanceTileClicked() {
         this.env.services.action.doAction({
@@ -109,6 +161,50 @@ class EmployeeDashboard extends Component {
         });
     }
 
+    //    function for fetching data when click on employee tile
+    async employeeTileClicked(e) {
+        var mmm = e.target.lastChild.innerText
+        var employ_id = parseInt(e.target.lastChild.innerText)
+        var employ_name = e.target.innerText
+
+        const employee_id_rec = await this.orm.searchRead('hr.employee', [
+            ['id', '=', employ_id]
+        ], ['user_id']);
+        var user_id = parseInt(employee_id_rec[0].user_id[0]);
+        const employee_job_rec = await this.orm.searchRead('hr.employee', [
+            ['id', '=', employ_id]
+        ], ['job_title']);
+        var job_title = employee_job_rec[0].job_title;
+
+        const employee_join_date_rec = await this.orm.searchRead('hr.employee', [
+            ['id', '=', employ_id]
+        ], ['create_date']);
+        var join_date = parseInt(employee_join_date_rec[0].create_date.slice(0, 4))
+        var employee_experience = new Date().getFullYear() - join_date
+
+        //        finding and replacing attendance count
+        var attendance = await this.orm.searchCount('hr.attendance', [
+            ["employee_id", "=", employ_id]
+        ]);
+        document.getElementById('attendance_count').innerHTML = attendance;
+        //        finding and replacing attendance count
+        var leave = await this.orm.searchCount('hr.leave', [
+            ["employee_id", "=", employ_id]
+        ]);
+        document.getElementById('leave_count').innerHTML = leave;
+        //        finding and replacing attendance count
+        var project = await this.orm.searchCount('project.project', [
+            ["user_id", "=", user_id]
+        ]);
+        document.getElementById('project_count').innerHTML = project;
+        //        replacing employee name, job title and experience
+        document.getElementById('employee_name').innerHTML = employ_name;
+        document.getElementById('employee_job_title').innerHTML = job_title;
+        document.getElementById('year_of_experience').innerHTML = `Experience : ${employee_experience} Years`;
+
+//        this.attendanceTileClicked(employ_id);
+
+    }
 }
 
 EmployeeDashboard.template = "employee_dashboard.EmployeeDashboard";
